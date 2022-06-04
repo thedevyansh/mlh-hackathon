609b770ffe2a435cc3ae23c7bd6167c7cf79412c
thedevyansh <schawla333333@gmail.com>
2022-06-04 13:08:59 +0530

    Add services for making api calls to backend

---
+++ frontend/src/services/rooms.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL:
+    !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000'
+      : 'https://acoustics.herokuapp.com',
+  withCredentials: true,
+});
+
+export const get = params => {
+  return instance.get('/api/rooms', { params });
+};
+
+export const update = (roomId, request) => {
+  return instance.put(`/api/rooms/update/${roomId}`, request);
+};
---
+++ frontend/src/services/user.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL:
+    !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000'
+      : 'https://acoustics.herokuapp.com',
+  withCredentials: true,
+});
+
+export const auth = () => {
+  return instance.get('/api/auth');
+};
+
+export const login = request => {
+  return instance.post('/api/auth/login', request);
+};
+
+export const register = request => {
+  return instance.post('/api/auth/register', request);
+};
+
+export const logout = () => {
+  return instance.post('/api/auth/logout');
+};
+
+export const update = request => {
+  return instance.put('/api/auth/update', request);
+};
